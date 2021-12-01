48d061b4be006f2aed4a4e796507d092ac503244
xcnwf <xcnwf@prontonmail.com>
2021-12-01 13:28:03 +0100

    d1-2 done

--- Solutions (rust)/d1.rs
+++ Solutions (rust)/d1.rs
@@ -28,13 +28,36 @@ fn main() {
     let mut nIncr = 0;
     let mut prevValue = 0;
 
+    let mut v1 = 0; let mut v2 = 0; let mut v3 = 0;
+    let mut nGroupIncr = 0;
+
     for (i,l) in lines.iter().filter(|&e| !e.is_empty()).enumerate() {
+
         let value = l.parse::<u32>().unwrap();
+        
+        //part 1
         if i>0 && value > prevValue {
             nIncr += 1;
         }
         prevValue = value;
+
+        //part2
+        if i == 0 {
+            v1 = value;
+        } else if i==1{
+            v2 = value;
+        } else if i == 2 {
+            v3 = value;
+        } else {
+            if value+v2+v3 > v1+v2+v3 {
+                nGroupIncr += 1;
+            }
+            v1 = v2;
+            v2 = v3;
+            v3 = value;
+        }
     }
 
     println!("First challenge: nIncr = {}",nIncr);
+    println!("Second challenge : nIncr = {}", nGroupIncr);
 }
