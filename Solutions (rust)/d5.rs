82a3e4507386dcc922488656d96a34a57a905bb6
xcnwf <xcnwf@prontonmail.com>
2021-12-05 12:20:07 +0100

    d5-2 done

--- Solutions (rust)/d5.rs
+++ Solutions (rust)/d5.rs
@@ -35,6 +35,8 @@ fn main() {
 
 	//overlaps is the list of points with their overlap count > 0
 	let mut overlaps: std::collections::HashMap<Point, usize> = std::collections::HashMap::new();
+
+	//part1: Parse horizontal and vertical lines
 	for line in content.lines() {
 		let mut points = line.split(" -> ");
 		let mut end_points: [(usize, usize);2] = [(0,0); 2];
@@ -44,6 +46,7 @@ fn main() {
 			let c2 = coordinate_iter.next().unwrap().parse::<usize>().unwrap();
 			end_points[i] = (c1,c2);
 		}
+
 
 		//Vertical lines
 		if end_points[0].0 == end_points[1].0 {
@@ -74,11 +77,44 @@ fn main() {
 			}
 		}
 	}
-
 	//PARSING DONE!
 
-	//part1 : For each overlap point, check if overlap > 1
-
+	//count overlaping parts
 	let n_overlaps = overlaps.values().filter(|&c| (*c)>1).count();
 	println!("First challenge - Result: {}", n_overlaps);
+	
+
+
+	//part2: add diagonals
+	for line in content.lines() {
+		let mut points = line.split(" -> ");
+		let mut end_points: [(usize, usize);2] = [(0,0); 2];
+		for i in 0..2 {
+			let mut coordinate_iter = points.next().unwrap().split(',');
+			let c1 = coordinate_iter.next().unwrap().parse::<usize>().unwrap();
+			let c2 = coordinate_iter.next().unwrap().parse::<usize>().unwrap();
+			end_points[i] = (c1,c2);
+		}
+
+
+		//diagonals
+		let (p1_x, p1_y) = end_points[0];
+		let (p2_x, p2_y) = end_points[1];
+		if p1_x != p2_x && p1_y != p2_y {
+			let (x_dep, y_dep, y_arr, length) = if p1_x > p2_x {(p2_x,p2_y,p1_y, p1_x-p2_x)} else {(p1_x,p1_y,p2_y,p2_x-p1_x)};
+			for i in 0..(length+1) {
+				let point = (x_dep+i, if y_arr > y_dep {y_dep+i} else {y_dep-i});
+				if !overlaps.contains_key(&point) {
+					overlaps.insert(point, 1);
+				} else {
+					*overlaps.get_mut(&point).unwrap() += 1;
+				}
+			}
+		}
+	}
+	//PARSING DONE!
+
+	//count overlaping parts
+	let n_overlaps = overlaps.values().filter(|&c| (*c)>1).count();
+	println!("Second challenge - Result: {}", n_overlaps); //18445 < R < 18878
 }
