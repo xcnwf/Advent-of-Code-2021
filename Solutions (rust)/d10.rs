cc09ac53c8e300195e17075d20b0a2fbf89f9c0e
xcnwf <xcnwf@prontonmail.com>
2021-12-09 23:31:28 +0100

    d10 init

---
+++ Solutions (rust)/d10.rs
@@ -0,0 +1,27 @@
+// d10.rs
+use std::fs;
+
+const DAY: &'static str = "d10";
+
+fn read_file() -> String {
+	print!("Reading input...");
+
+	let content: std::io::Result<String> = fs::read_to_string("../Inputs/".to_owned()+DAY+".txt");
+
+	println!("Done!");
+
+	if let Ok(s) = content {
+		s
+	} else {
+		panic!("File Reading Error! Aborting!")
+	}
+}
+
+fn main() {
+	println!("Day {}\n", DAY);
+
+	let content = read_file();
+
+	// Day 10 Challenge : ??
+
+}
