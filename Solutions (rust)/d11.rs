6baed48bf29a22de92cecf680b3a5f701804a0f6
xcnwf <xcnwf@prontonmail.com>
2021-12-11 12:31:34 +0100

    d11 init

---
+++ Solutions (rust)/d11.rs
@@ -0,0 +1,26 @@
+// d11.rs
+use std::fs;
+
+const DAY: &'static str = "d11_example";
+
+fn read_file() -> String {
+	print!("Reading input...");
+
+	let content: std::io::Result<String> = fs::read_to_string("../Inputs/".to_owned()+DAY+".txt");
+
+	println!("Done!");
+
+	if let Ok(s) = content {
+		s
+	} else {
+		panic!("File Reading Error! Aborting!")
+	}
+}
+
+fn main() {
+	println!("Day {}\n", DAY);
+
+	let content = read_file();
+
+	// Day 11 Challenge : ??
+}
