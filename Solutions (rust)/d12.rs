daf0d07c079310387e25c7e719e0e1151e1e648d
xcnwf <xcnwf@prontonmail.com>
2021-12-12 14:07:59 +0100

    d12-1 done

--- Solutions (rust)/d12.rs
+++ Solutions (rust)/d12.rs
@@ -1,8 +1,7 @@
 // d12.rs
 use std::fs;
 
-const DAY: &'static str = "d12_example";
-const SIZE: usize = 10;
+const DAY: &'static str = "d12";
 
 fn read_file() -> String {
 	print!("Reading input...");
@@ -25,4 +24,64 @@ fn main() {
 
 	// Day 12 Challenge : ??
 
+    let mut links: std::collections::HashMap<&str, Vec<&str>>  = std::collections::HashMap::new();
+
+    for line in content.lines() {
+        let mut spots = line.split("-");
+        let start = spots.next().unwrap();
+        if !links.contains_key(&start) {
+            links.insert(start, Vec::new());
+        }
+        let end = spots.next().unwrap();
+        if !links.contains_key(end) {
+            links.insert(end, Vec::new());
+        }
+
+        links.get_mut(start).unwrap().push(end);
+        links.get_mut(end).unwrap().push(start);
+    }
+
+    // part 1;
+    let mut visits: Vec<Vec<&str>> = Vec::new();
+    let start = vec!("start");
+    visits.push(start);
+
+    let mut total_paths: usize = 0;
+
+    // while there are still things to visit.
+    // /!\ no need to do further checks : big caves are never linked together,
+    // removing any risk of infinite looping
+    while visits.len() > 0 {
+        let curr_visit: Vec<&str> = visits.pop().unwrap();
+        for &cave in links[curr_visit.last().unwrap()].iter() {
+            // if we reached the end, count the path
+            if cave == "end" {
+                total_paths += 1;
+                continue;
+            }
+
+            // skip the small caves already visited
+            if is_lowercase(cave) {
+                if curr_visit.contains(&cave){
+                    continue;
+                }
+            }
+
+            //add all the new routes to visit.
+            let mut new_visit = curr_visit.clone();
+            new_visit.push(cave);
+            visits.push(new_visit);
+        }
+    }
+
+    println!("First challenge - result: {}",total_paths);
+
+}
+
+fn is_lowercase<'a>(s: &'a str) -> bool {
+    let mut result: bool = true;
+    for c in s.chars() {
+        result = result && c.is_ascii_lowercase();
+    }
+    result
 }
